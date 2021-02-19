5a53a0cece9ae847161ca7c728d24cb08bfccb3e
stefi23 <stefania.rosca2309@gmail.com>
2021-02-19 22:19:41 +0100

    simple counter with useReducer

---
+++ src/components/Counter.js
@@ -0,0 +1,38 @@
+import React, {useReducer} from 'react'
+import {Button, Badge} from 'reactstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const initialState = 0
+
+const reducer = (state, action) => {
+    switch(action){
+        case 'increment':
+            return state + 1
+        case 'decrement':
+            return state - 1
+        case 'reset':
+            return initialState
+        default:
+            return state;
+    }
+}
+
+const Counter = () => {
+    const [count, dispatch] = useReducer(reducer, initialState)
+
+    return(
+        <>  
+            <div className='counter'>
+                Counter <Badge color="info">{count}</Badge>
+            </div>
+
+             <div>
+                <Button color="secondary" onClick={() => dispatch('reset')}>Reset</Button>
+                <Button color="danger" onClick={() => dispatch('decrement')}>Decrement</Button>
+                <Button color="success"onClick={() => dispatch('increment')}>Increment</Button>
+            </div>
+        </>
+    )
+}
+
+export default Counter
